402bedc36ab31dce00bb19c4fa82de5a2de8625e
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-06-30 21:52:45 +0100

    feat: Create trapezium shape component

---
+++ src/components/trapezium.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+const Trapezium = () => {
+    return (
+        <Box 
+            position="absolute"
+            top="0"
+            left="0"
+            width="28%"
+            height='100%'
+            bg='transparent'
+            zIndex='0'
+            hideBelow='lg'
+            _before={{
+                content: '""',
+                position: 'absolute',
+                top: '0',
+                left: '0',
+                width: '100%',
+                height: '100%',
+                bg: '#daa520',
+                clipPath: 'polygon(0% 0%, 50% 0%, 100% 100%, 0% 100%)'
+            }}
+        />
+    );
+};
+
+export default Trapezium;
