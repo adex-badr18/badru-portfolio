2563fbd53e48abdca038b5da3a62e6ed24f82cf6
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-07-03 11:22:31 +0100

    feat: Create DataField component

---
+++ src/components/about/dataField.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Flex, Text } from "@chakra-ui/react";
+
+const DataField = ({ fieldName, value }) => {
+    return (
+        <Flex
+            direction={{ base: "column", sm: "row", lg: "column", xl: "row" }}
+            gap={{ base: 0, sm: 2, lg: 0, xl: 2 }}
+        >
+            <Text opacity="0.8">{`${fieldName}:`}</Text>
+            <Text fontWeight="semibold">{value}</Text>
+        </Flex>
+    );
+};
+
+export default DataField;
